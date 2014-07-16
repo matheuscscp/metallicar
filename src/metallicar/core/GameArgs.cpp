81952654a0e463b7b495e2d05933356def0a7ad8
Pimenta <matheuscscp@gmail.com>
2014-07-16 00:01:44 -0300

    Changing void* to a class pointer

---
+++ src/metallicar/core/GameArgs.cpp
@@ -0,0 +1,21 @@
+/*
+ * GameArgs.cpp
+ *
+ *  Created on: Jul 15, 2014
+ *      Author: Pimenta
+ */
+
+// this
+#include "metallicar_core.hpp"
+
+namespace metallicar {
+
+GameArgs::GameArgs() {
+  
+}
+
+GameArgs::~GameArgs() {
+  
+}
+
+} // namespace metallicar
